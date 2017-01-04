40e08d90a33fafd4d76efbd3aef908ca9d9b6e68
OKAR OU <okar.ou@oocl.com>
2017-01-04 11:29:26 +0800

    first commit

---
+++ OKUtil/Classes/OKUtil.h
@@ -0,0 +1,13 @@
+//
+//  OKUtil.h
+//  Pods
+//
+//  Created by OKAR OU on 17/1/4.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OKUtil : NSObject
+
+@end
---
+++ OKUtil/Classes/OKUtil.m
@@ -0,0 +1,13 @@
+//
+//  OKUtil.m
+//  Pods
+//
+//  Created by OKAR OU on 17/1/4.
+//
+//
+
+#import "OKUtil.h"
+
+@implementation OKUtil
+
+@end
